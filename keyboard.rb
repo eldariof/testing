a2b7d0a1deff5a85ac17ebfbe26ee687e299cd47
Eldar Abdrazakov <e.abdrazakov@cti.ru>
2011-11-14 11:51:02 +0300

    Added keyboard code

--- keyboard.rb
+++ keyboard.rb
@@ -2,16 +2,43 @@
 
 module Keyboard
 
-  NUMB1 = "\xEE\x80\xB3"
-  NUMB2 = "\xEE\x80\xB4"
-  NUMB3 = "\xEE\x80\xB5"
-  NUMB4 = "\xEE\x80\xB6"
-  NUMB5 = "\xEE\x80\xB7"
-  NUMB6 = "\xEE\x80\xB8"
-  NUMB7 = "\xEE\x80\xB9"
-  NUMB8 = "\xEE\x80\xBA"
-  NUMB9 = "\xEE\x80\xBB"
-  ENTER = "\xEE\x80\x86"
+  NUMB1         = "\xEE\x80\xB3"
+  NUMB2         = "\xEE\x80\xB4"
+  NUMB3         = "\xEE\x80\xB5"
+  NUMB4         = "\xEE\x80\xB6"
+  NUMB5         = "\xEE\x80\xB7"
+  NUMB6         = "\xEE\x80\xB8"
+  NUMB7         = "\xEE\x80\xB9"
+  NUMB8         = "\xEE\x80\xBA"
+  NUMB9         = "\xEE\x80\xBB"
+  ENTER         = "\xEE\x80\x86"
+  #ENTER         = "\xEE\x81\x81"      57409
+  MENU          = "\xEE\x80\x87"
+  DELETE        = "\xEE\x80\x85"
+  UP            = "\xEE\x80\x8E"
+  DOWN          = "\xEE\x80\x8F"
+  PAGE_UP       = "\xEE\x80\x90"
+  PAGE_DOWN     = "\xEE\x80\x91"
+  LEFT          = "\xEE\x80\x92"
+  RIGHT         = "\xEE\x80\x93"
+  CHANNEL_PLUS  = "\xEE\x80\x94"
+  CHANNEL_MINUS = "\xEE\x80\x95"
+  VOLUME_PLUS   = "\xEE\x80\x96"
+  VOLUME_MINUS  = "\xEE\x80\x97"
+  MUTE          = "\xEE\x80\x8B"
+  INFO          = "\xEE\x80\x81"
+  REBOOT        = "\xEE\x80\x82"
+  HOME          = "\xEE\x80\x83"
+  REWIND        = "\xEE\x80\x84"
+  PLAY          = "\xEE\x80\xB1"
+  PAUSE         = "\xEE\x80\xB2"
+  FORWARD       = "\xEE\x80\xBC"
+  STOP          = "\xEE\x81\x86"
+  RED           = "\xEE\x81\x87"
+  GREEN         = "\xEE\x81\x85"
+  YELLOW        = "\xEE\x81\x84"
+  BLUE          = "\xEE\x81\x83"
+  EPG           = "\xEE\x81\x82"
 
   PLATFORM_CODES = "fw.platform.KeyCodes[114]='1';
                     fw.platform.KeyCodes[115]='2';
@@ -22,6 +49,32 @@ module Keyboard
                     fw.platform.KeyCodes[120]='7';
                     fw.platform.KeyCodes[121]='8';
                     fw.platform.KeyCodes[122]='9';
-                    fw.platform.KeyCodes[13]='ENTER'"
+                    fw.platform.KeyCodes[13]='ENTER';
+                    fw.platform.KeyCodes[14]='MENU';
+                    fw.platform.KeyCodes[12]='DELETE';
+                    fw.platform.KeyCodes[33]='UP';
+                    fw.platform.KeyCodes[34]='DOWN';
+                    fw.platform.KeyCodes[35]='PAGE_UP';
+                    fw.platform.KeyCodes[36]='PAGE_DOWN';
+                    fw.platform.KeyCodes[37]='LEFT';
+                    fw.platform.KeyCodes[38]='RIGHT';
+                    fw.platform.KeyCodes[39]='CHANNEL_PLUS';
+                    fw.platform.KeyCodes[40]='CHANNEL_MINUS';
+                    fw.platform.KeyCodes[45]='VOLUME_PLUS';
+                    fw.platform.KeyCodes[46]='VOLUME_MINUS';
+                    fw.platform.KeyCodes[19]='MUTE';
+                    fw.platform.KeyCodes[3]='INFO';
+                    fw.platform.KeyCodes[6]='REBOOT';
+                    fw.platform.KeyCodes[8]='HOME';
+                    fw.platform.KeyCodes[9]='REWIND';
+                    fw.platform.KeyCodes[112]='PLAY';
+                    fw.platform.KeyCodes[113]='PAUSE';
+                    fw.platform.KeyCodes[123]='FORWARD';
+                    fw.platform.KeyCodes[57414]='STOP';
+                    fw.platform.KeyCodes[57415]='RED';
+                    fw.platform.KeyCodes[57413]='GREEN';
+                    fw.platform.KeyCodes[57412]='YELLOW';
+                    fw.platform.KeyCodes[57411]='BLUE';
+                    fw.platform.KeyCodes[57410]='EPG'"
 
 end
